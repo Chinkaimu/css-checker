81e11ace23a210dbbf5ffb24bef061143f696eb8
chinkaimu <823388255@qq.com>
2020-03-28 23:25:49 +0800

    feat: finish primitive checker

--- README.md
+++ README.md
@@ -15,17 +15,17 @@
   ```
 
 ## 功能流程
-* 读取当前目录下所有 CSS 文件路径
-    1. 增强：Sass、Less 文件读取
-* 读取每个 CSS 文件内容
+* match all the paths of CSS
+    1. to enhance：adapt to Sass、Less
+* read the css file
 * PostCss 解析，找到 CSS 文件内的 selector
 * 判断 selector 是否标签
     1. 增强：检查类型
     2. 增强：配置要检查内容（读取命令行参数)
 
-## 工具
-* [tiny-glob](https://github.com/terkelg/tiny-glob)
-* [PostCss](https://postcss.org/)
+## Tools
+* [tiny-glob](https://github.com/terkelg/tiny-glob): match the paths
+* [PostCss](https://postcss.org/): transform CSS with JavaScript
 
 
 
--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 #!/usr/bin/env node
 const glob = require('tiny-glob');
 const fs = require('fs').promises;
+const postcss = require('postcss');
 
 (async function () {
-  let paths = await glob('src/**/*.{css}')
+  let paths = await glob('code-projects/**/*.{css}')
+  let checkResult = null
 
+  const readPromises = []
   for (const path of paths) {
-    const content = await fs.readFile(path, { encoding: 'utf-8' });
-
-    console.log(content)
+    // add path at callback
+    readPromises.push(fs.readFile(path, { encoding: 'utf-8' }).then(content => ({ path, content })));
   }
+  const fileContents = await Promise.all(readPromises)
+
+  const checkPromises = []
+  for (const { path, content } of fileContents) {
+    checkPromises.push(checkOneFile(path, content))
+  }
+  checkResult = await Promise.all(checkPromises)
+
+  console.log(checkResult)
 })();
+
+async function checkOneFile (path, content) {
+  return new Promise((resolve, reject) => {
+    const nodes = postcss.parse(content).nodes
+    if (!nodes) {
+      reject()
+    }
+    const data = {}
+    data[path] = 'pass the check'
+
+    for (const node of nodes) {
+      const selector = node.selector
+      if (selector && !selector.startsWith('.') && !selector.startsWith('#')) {
+        const start = node.source.start
+        data[path] = `there is tag selector in ${start.line}:${start.column}`
+      }
+    }
+
+    resolve(data)
+  })
+}
 
